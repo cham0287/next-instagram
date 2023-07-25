4edcb610546e4b180e7691f9823418cc490b9e19
cham0287 <crtmt97@gmail.com>
2023-07-26 00:04:48 +0900

    Feat: define user, post schema

---
+++ server/sanity-studio/schemas/post.js
@@ -0,0 +1,37 @@
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+}
