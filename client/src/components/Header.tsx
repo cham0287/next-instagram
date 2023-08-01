51892bbbe4c7ecb4ded84bb90d22a59702660673
cham0287 <crtmt97@gmail.com>
2023-08-02 00:09:44 +0900

    Refactor: nav menu map 반복문으로 개선

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,31 @@
 'use client';
 import Link from 'next/link';
 import React from 'react';
-import { AiOutlineHome, AiFillHome } from 'react-icons/ai';
-import { RiSearchLine, RiSearchFill } from 'react-icons/ri';
-import { BsPlusSquare, BsPlusSquareFill } from 'react-icons/bs';
 import { usePathname } from 'next/navigation';
+import HomeIcon from './ui/icons/HomeIcon';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+
+const menu = [
+  {
+    href: '/',
+    icon: <HomeIcon />,
+    clickedIcon: <HomeFillIcon />,
+  },
+  {
+    href: '/search',
+    icon: <SearchIcon />,
+    clickedIcon: <SearchFillIcon />,
+  },
+  {
+    href: '/new',
+    icon: <NewIcon />,
+    clickedIcon: <NewFillIcon />,
+  },
+];
 
 const Header = () => {
   const pathname = usePathname();
@@ -12,15 +33,11 @@ const Header = () => {
     <div className='flex items-center justify-between mx-auto p-4 border border-b-gray-300 border-b-4'>
       <h1 className='text-3xl font-bold'>Instantgram</h1>
       <ul className='flex items-center gap-6 text-4xl'>
-        <Link href='/'>
-          {pathname === '/' ? <AiFillHome /> : <AiOutlineHome />}
-        </Link>
-        <Link href='/search'>
-          {pathname === '/search' ? <RiSearchFill /> : <RiSearchLine />}
-        </Link>
-        <Link href='/new'>
-          {pathname === '/new' ? <BsPlusSquareFill /> : <BsPlusSquare />}
-        </Link>
+        {menu.map(({ href, icon, clickedIcon }) => (
+          <li key={href}>
+            <Link href={href}>{href === pathname ? clickedIcon : icon}</Link>
+          </li>
+        ))}
         <li>
           <p className='px-1 py-2 border-4 border-purple-500 rounded-md text-xl'>
             Sign in
