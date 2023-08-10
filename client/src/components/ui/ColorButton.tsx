6b81185f652c29f6acf9efd30e275424a1b55387
cham0287 <crtmt97@gmail.com>
2023-08-10 23:38:48 +0900

    Feat: ColorButton size prop 추가

--- client/src/components/ui/ColorButton.tsx
+++ client/src/components/ui/ColorButton.tsx
@@ -1,13 +1,19 @@
 interface Props {
   text: string;
   onClick: () => void;
+  size?: 'small' | 'big';
 }
 
-const ColorButton = ({ text, onClick }: Props) => {
+const ColorButton = ({ text, onClick, size = 'small' }: Props) => {
   return (
-    <div className='rounded-md bg-gradient-to-br from-fuchsia-600 via-rose-500 to-amber-300 p-1'>
+    <div
+      className={`rounded-md bg-gradient-to-br from-fuchsia-600 via-rose-500 to-amber-300 p-1
+      ${size === 'big' ? 'p-[0.3rem]' : 'p-[0.15rem]'}`}
+    >
       <button
-        className='bg-white rounded-sm text-base p-[0.3rem] hover:opacity-90 transition-opacity'
+        className={`bg-white rounded-sm text-base p-[0.3rem] hover:opacity-90 transition-opacity
+        ${size === 'big' ? 'p-4 text-2xl' : 'p-[0.3rem] text-base'}
+        `}
         onClick={onClick}
       >
         {text}
