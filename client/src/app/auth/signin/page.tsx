5a5ffb74ef237b8db6db0852a27c4a67ed5ef223
cham0287 <crtmt97@gmail.com>
2023-08-11 00:03:59 +0900

    Feat: callBackUrl 전달

--- client/src/app/auth/signin/page.tsx
+++ client/src/app/auth/signin/page.tsx
@@ -5,18 +5,25 @@ import { getProviders } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import Signin from '@/components/Signin';
 
-export default async function SignInPage() {
+interface Props {
+  searchParams: {
+    callbackUrl: string;
+  };
+}
+
+export default async function SignInPage({
+  searchParams: { callbackUrl },
+}: Props) {
   const session = await getServerSession(authOptions);
 
   if (session) {
     redirect('/');
   }
-
   const providers = (await getProviders()) ?? {};
 
   return (
     <section className='flex justify-center mt-[30%]'>
-      <Signin providers={providers} />
+      <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
     </section>
   );
 }
