4dbb41acf33ec80aea1bcc491a3e7328f73b8438
cham0287 <crtmt97@gmail.com>
2023-08-11 00:06:29 +0900

    Feat: callBackUrl 전달2

--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -2,18 +2,19 @@
 import { ClientSafeProvider, signIn } from 'next-auth/react';
 import ColorButton from './ui/ColorButton';
 
-type Props = {
+interface Props {
   providers: Record<string, ClientSafeProvider>;
-};
+  callbackUrl: string;
+}
 
-const Signin = ({ providers }: Props) => {
+const Signin = ({ providers, callbackUrl }: Props) => {
   return (
     <>
       {Object.values(providers).map(({ name, id }) => (
         <ColorButton
           key={id}
           text={`Sign In With ${name}`}
-          onClick={() => signIn(id)}
+          onClick={() => signIn(id, { callbackUrl })}
           size='big'
         />
       ))}
