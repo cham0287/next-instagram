b6deca57789354582d99987428ddfebc5bc83522
cham0287 <crtmt97@gmail.com>
2023-08-14 22:47:57 +0900

    Feat: Avatar size prop 추가

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 interface Props {
   image?: string | null;
+  size?: 'big' | 'small';
 }
 
-const Avatar = ({ image }: Props) => {
+const Avatar = ({ image, size = 'small' }: Props) => {
   return (
-    <div className='w-9 h-9 rounded-full bg-gradient-to-br from-fuchsia-600 via-rose-500 to-amber-300'>
+    <div
+      className={`${
+        size === 'small' ? 'w-9 h-9' : 'w-12 h-12'
+      } rounded-full bg-gradient-to-br from-fuchsia-600 via-rose-500 to-amber-300`}
+    >
       {/*  eslint-disable-next-line @next/next/no-img-element */}
       <img
         className='rounded-full p-[0.1rem]'
