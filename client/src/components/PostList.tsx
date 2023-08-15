20ee0bfc3d14572fccd17274cd7678f83fb0a6ca
cham0287 <crtmt97@gmail.com>
2023-08-16 00:23:22 +0900

    Feat: PostList 및 PostCard 생성

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,12 +1,29 @@
 'use client';
 import { SimplePost } from '@/model/post';
+import { GridLoader } from 'react-spinners';
 import useSWR from 'swr';
+import PostCard from './PostCard';
 
 const PostList = () => {
   const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
-  console.log(posts);
   return (
-    <p>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</p>
+    <section>
+      {isLoading && (
+        <div>
+          <GridLoader color='red' />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.text}
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 };
 
