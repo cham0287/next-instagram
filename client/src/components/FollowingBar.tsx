3562c5213bef86781833171282bc24a10cb20f6a
Donghyun Kim <crtmt97@gmail.com>
2023-08-15 17:11:44 +0900

    Feat: followingBar 추가, SWR 추가 및 전역 ConfigContext 생성 (#6)
    * Feat: SWR 추가 및 SWRConfig setting
    
    * Feat: user 관련 데이터 api 추가 및 연결
    
    * Feat: api/me 타입 지정
    
    * Feat: FollowingBar 및 loading spinner 추가
    
    * Design: 홈 레이아웃 수정
    
    * Feat: react-multi-carousel 적용

--- client/src/components/FollowingBar.tsx
+++ client/src/components/FollowingBar.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+'use client';
+import useSWR from 'swr';
+import { DetailUser } from '@/model/user';
+import { PropagateLoader } from 'react-spinners';
+import Link from 'next/link';
+import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 const FollowingBar = () => {
-  return <p>followingBar</p>;
+  const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+  return (
+    <section className='w-full flex justify-center items-center p-4 shadow-md shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
+      {isLoading ? (
+        <PropagateLoader size={8} color='red' />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 };
-
 export default FollowingBar;
--- client/src/service/user.ts
+++ client/src/service/user.ts
@@ -1,6 +1,6 @@
 import { client } from './sanity';
 
-interface OAUthUser {
+interface OAuthUser {
   id: string;
   email: string;
   name: string;
@@ -8,7 +8,7 @@ interface OAUthUser {
   image?: string | null;
 }
 
-export async function addUser({ id, username, email, name, image }: OAUthUser) {
+export async function addUser({ id, username, email, name, image }: OAuthUser) {
   return client.createIfNotExists({
     _id: id,
     _type: 'user',
@@ -20,4 +20,16 @@ export async function addUser({ id, username, email, name, image }: OAUthUser) {
     followers: [],
     bookmarks: [],
   });
+}
+
+export async function getUserByUsername(username: string) {
+  return client.fetch(
+    `*[_type == "user" && username == "${username}"][0]{
+      ...,
+      "id":_id,
+      following[]->{username,image},
+      followers[]->{username,image},
+      "bookmarks":bookmarks[]->_id
+    }`
+  );
 }
