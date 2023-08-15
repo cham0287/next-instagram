2c60975d036b533923acb384f9e7d6516bbc6d0b
cham0287 <crtmt97@gmail.com>
2023-08-15 17:10:38 +0900

    Feat: react-multi-carousel 적용

--- client/src/components/FollowingBar.tsx
+++ client/src/components/FollowingBar.tsx
@@ -4,10 +4,24 @@ import { DetailUser } from '@/model/user';
 import { PropagateLoader } from 'react-spinners';
 import Link from 'next/link';
 import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 const FollowingBar = () => {
   const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
-  const users = data?.following;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
   return (
     <section className='w-full flex justify-center items-center p-4 shadow-md shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
       {isLoading ? (
@@ -16,21 +30,20 @@ const FollowingBar = () => {
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul className='w-full flex gap-2'>
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link
-                className='flex flex-col items-center w-20'
-                href={`/user/${username}`}
-              >
-                <Avatar image={image} highlight />
-                <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
-                  {username}
-                </p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
