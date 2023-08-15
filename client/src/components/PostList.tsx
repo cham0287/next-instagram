cfe7073c27bf85b4c7568bcd4252ace3d0b50d9a
Donghyun Kim <crtmt97@gmail.com>
2023-08-16 00:41:12 +0900

    Feat: PostList, PostCard 추가, post api 생성 및 연결 (#7)
    * Feat: following posts api, query, type 추가
    
    * Chore: 미사용 라인 제거
    
    * Feat: sanity imgUrl 추가, url 내부 데이터 가공
    
    * Chore: Add Icons
    
    * Chore: nextConfig sanity images 설정
    
    * Feat: PostList 및 PostCard 생성
    
    * Refactor: Avatar component 확장성, 타입 안정성 개선
    
    * Design: PostCard 스타일링

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+'use client';
+import { SimplePost } from '@/model/post';
+import { GridLoader } from 'react-spinners';
+import useSWR from 'swr';
+import PostCard from './PostCard';
 
 const PostList = () => {
-  return <p>post list</p>;
+  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
+  return (
+    <section>
+      {isLoading && (
+        <div className='text-center mt-32'>
+          <GridLoader color='red' />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id} className='mb-4'>
+              {post.text}
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default PostList;
