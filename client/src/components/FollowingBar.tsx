02ebe8fdaacd497d1cc780efa80b0ee65fef58da
cham0287 <crtmt97@gmail.com>
2023-08-15 16:21:13 +0900

    Feat: FollowingBar 및 loading spinner 추가

--- client/src/components/FollowingBar.tsx
+++ client/src/components/FollowingBar.tsx
@@ -1,11 +1,38 @@
 'use client';
 import useSWR from 'swr';
 import { DetailUser } from '@/model/user';
+import { PropagateLoader } from 'react-spinners';
+import Link from 'next/link';
+import Avatar from './Avatar';
 
 const FollowingBar = () => {
   const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
-  console.log(data);
-  return <p>followingBar</p>;
+  const users = data?.following;
+  return (
+    <section className='w-full flex justify-center items-center p-4 shadow-md shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
+      {isLoading ? (
+        <PropagateLoader size={8} color='red' />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul className='w-full flex gap-2'>
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link
+                className='flex flex-col items-center w-20'
+                href={`/user/${username}`}
+              >
+                <Avatar image={image} highlight />
+                <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                  {username}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
-
 export default FollowingBar;
