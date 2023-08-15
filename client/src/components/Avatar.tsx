86f4b60cf74a6a65d25d1b03698c6cf4d27c9e9e
cham0287 <crtmt97@gmail.com>
2023-08-16 00:31:55 +0900

    Refactor: Avatar component 확장성, 타입 안정성 개선

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-
+type AvatarSize = 'big' | 'medium' | 'small';
 interface Props {
   image?: string | null;
-  size?: 'big' | 'small';
+  size?: AvatarSize;
   highlight?: boolean;
 }
 
@@ -22,16 +21,38 @@ const Avatar = ({ image, size = 'big', highlight = false }: Props) => {
 
 export default Avatar;
 
-const getContainerStyle = (size: string, hightlight: boolean): string => {
+const getContainerSize = (size: AvatarSize): string => {
+  switch (size) {
+    case 'small':
+      return 'w-9 h-9';
+    case 'medium':
+      return 'w-11 h-11';
+    case 'big':
+      return 'w-[68px] h-[68px]';
+    default:
+      return 'w-[68px] h-[68px]';
+  }
+};
+
+const getImageSizeStyle = (size: AvatarSize): string => {
+  switch (size) {
+    case 'small':
+      return 'w-[34px] h-[34px] p-[0.1rem]';
+    case 'medium':
+      return 'w-[42px] h-[42px] p-[0.1rem]';
+    case 'big':
+      return 'w-16 h-16 p-[0.2rem]';
+    default:
+      return 'w-16 h-16 p-[0.2rem]';
+  }
+};
+
+const getContainerStyle = (size: AvatarSize, hightlight: boolean): string => {
   const baseStyle = 'rounded-full flex justify-center items-center';
   const highlightStyle = hightlight
     ? 'bg-gradient-to-br from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
-  const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]';
+  const sizeStyle = getContainerSize(size);
 
   return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
-};
-
-const getImageSizeStyle = (size: string): string => {
-  return size === 'small' ? 'w-[34px] h-[34px]' : 'w-16 h-16 p-[0.2rem]';
 };
