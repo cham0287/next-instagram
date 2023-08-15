fb66e9804d372c3843a6503311f5f4f6203025b2
cham0287 <crtmt97@gmail.com>
2023-08-15 22:11:51 +0900

    Feat: sanity imgUrl 추가, url 내부 데이터 가공

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+'use client';
+import { SimplePost } from '@/model/post';
+import useSWR from 'swr';
 
 const PostList = () => {
-  return <p>post list</p>;
+  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
+  console.log(posts);
+  return (
+    <p>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</p>
+  );
 };
 
 export default PostList;
