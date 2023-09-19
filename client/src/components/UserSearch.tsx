d32ee91043abe7fcb71ab68b164d0a62b702b578
cham0287 <crtmt97@gmail.com>
2023-09-20 00:04:44 +0900

    Feat: 사용자 검색 ui 완성

--- client/src/components/UserSearch.tsx
+++ client/src/components/UserSearch.tsx
@@ -1,12 +1,47 @@
 'use client';
 
-import { useState } from 'react';
+import { ProfileUser } from '@/model/user';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
+import GridSpinner from './ui/GridSpinner';
 
 const UserSearch = () => {
   const [keyword, setKeyword] = useState('bob');
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
-  return <div></div>;
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          autoFocus
+          placeholder='Search for a username or name'
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>무언가가 잘못 되었습니다</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && (
+        <p>검색한 사용자가 없습니다</p>
+      )}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 export default UserSearch;
--- client/src/model/user.ts
+++ client/src/model/user.ts
@@ -12,3 +12,8 @@ export interface DetailUser extends User {
   followers: SimpleUser[];
   bookmarks: string[];
 }
+
+export interface ProfileUser extends User {
+  following: number;
+  followers: number;
+}
